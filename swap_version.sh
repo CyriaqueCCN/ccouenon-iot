4077576cfdc9e5f64f4716a34e58a6eabfcc0e47
iot <iot@debian>
2024-09-04 18:39:12 +0200

    added swap script

---
+++ swap_version.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if grep ':v1' p3/deployment.yaml ; then
+	sed -i 's/wil42\/playground\:v1/wil42\/playground\:v2/g' p3/deployment.yaml
+else
+	sed -i 's/wil42\/playground\:v2/wil42\/playground\:v1/g' p3/deployment.yaml
+fi
